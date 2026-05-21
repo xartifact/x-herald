805c1a22f4be21673256ed955e1120a8f3adaa6b
zbin <zbin@xartifact.dev>
2026-05-21 14:16:10 +0800

    chore: 移除 TanStack Start 实验脚手架

--- apps/tanstack/app/client.tsx
+++
@@ -1,7 +0,0 @@
-import { StartClient, mount } from '@tanstack/start/client'
-import { createRouter } from './router'
-
-mount('#root', () => {
-  const router = createRouter()
-  return <StartClient router={router} />
-})
